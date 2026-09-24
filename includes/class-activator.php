86d4b87dbe2e20e2cd387886c3ef02040954cc77
jalaldogonchi95 <56954671+jalaldogonchi95@users.noreply.github.com>
2026-09-24 22:25:47 +0330

    Make activation safe and minimal

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -3,13 +3,7 @@ defined( 'ABSPATH' ) || exit;
 
 final class REAI_Activator {
 	public static function activate() {
-		if ( class_exists( '\RealEstateAI\Core\Roles' ) ) {
-			\RealEstateAI\Core\Roles::register();
-		}
-		if ( class_exists( '\RealEstateAI\Core\PostTypes' ) ) {
-			\RealEstateAI\Core\PostTypes::register();
-		}
-		flush_rewrite_rules();
 		update_option( 'reai_version', REAI_VERSION );
+		flush_rewrite_rules();
 	}
 }
